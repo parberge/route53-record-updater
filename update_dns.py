edeb0713a1858ed005da2686d6901c39bb18ef1b
parberge <paer.berge@gmail.com>
2021-02-09 11:01:33 +0100

    Auto styling with black

--- update_dns.py
+++ update_dns.py
@@ -6,24 +6,24 @@ from helpers import get_env_variable
 log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
 
-zone_id = get_env_variable(variable='AWS_ZONE_ID')
-domain_name = get_env_variable(variable='AWS_DOMAIN_NAME')
-aws_access_key = get_env_variable(variable='AWS_ACCESS_KEY')
-aws_secret_key = get_env_variable(variable='AWS_SECRET_KEY')
+zone_id = get_env_variable(variable="AWS_ZONE_ID")
+domain_name = get_env_variable(variable="AWS_DOMAIN_NAME")
+aws_access_key = get_env_variable(variable="AWS_ACCESS_KEY")
+aws_secret_key = get_env_variable(variable="AWS_SECRET_KEY")
 log_level = get_env_variable(variable="LOG_LEVEL", default_value="INFO")
 log.setLevel(logging.getLevelName(log_level))
 
 
-public_ip_api = 'https://ipconfig.io'
+public_ip_api = "https://ipconfig.io"
 
-current_ip = requests.get(url=f'{public_ip_api}/json').json().get('ip')
+current_ip = requests.get(url=f"{public_ip_api}/json").json().get("ip")
 log.debug(f"Current IP: {current_ip}")
 
 if not current_ip:
     raise ValueError("Unable to get current IP")
 
 client = boto3.client(
-    'route53',
+    "route53",
     aws_access_key_id=aws_access_key,
     aws_secret_access_key=aws_secret_key,
 )
@@ -31,45 +31,45 @@ client = boto3.client(
 record_object = client.list_resource_record_sets(
     HostedZoneId=zone_id,
     StartRecordName=domain_name,
-    StartRecordType='A',
+    StartRecordType="A",
     MaxItems="1",
 )
 
-records = record_object.get('ResourceRecordSets')
+records = record_object.get("ResourceRecordSets")
 
 ip = None
 ttl = None
 for item in records:
     log.debug(f"Record object: {item}")
-    if item.get('Type') == 'A' and domain_name in item.get('Name'):
-        ttl = item.get('TTL')
-        ip_list = item.get('ResourceRecords')
-        ip = ip_list.pop().get('Value')
+    if item.get("Type") == "A" and domain_name in item.get("Name"):
+        ttl = item.get("TTL")
+        ip_list = item.get("ResourceRecords")
+        ip = ip_list.pop().get("Value")
         break
 else:
-    raise ValueError(f"Unable to get IP for route 53 domain {domain_name} (zone ID {zone_id})")
-    
+    raise ValueError(
+        f"Unable to get IP for route 53 domain {domain_name} (zone ID {zone_id})"
+    )
+
 if current_ip != ip:
     log.info(f"Current IP {current_ip} doesn't match IP {ip}")
 
     response = client.change_resource_record_sets(
         HostedZoneId=zone_id,
         ChangeBatch={
-            'Changes': [
+            "Changes": [
                 {
-                    'Action': 'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': domain_name,
-                        'Type': 'A',
-                        'TTL': ttl,
-                        'ResourceRecords': [
-                            {
-                                'Value': current_ip
-                            },
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": domain_name,
+                        "Type": "A",
+                        "TTL": ttl,
+                        "ResourceRecords": [
+                            {"Value": current_ip},
                         ],
-                    }
+                    },
                 },
             ]
-        }
+        },
     )
     log.info(response)
